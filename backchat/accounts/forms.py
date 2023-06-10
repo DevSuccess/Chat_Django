f989e0ff943bf0d4660e2179d80eb11e6f2ff5b5
ITDevSuccess <raharisontsidiany@gmail.com>
2023-06-10 22:46:26 +0300

    Ajout de style au code

--- backchat/accounts/forms.py
+++ backchat/accounts/forms.py
@@ -1,4 +1,5 @@
 from .models import User
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 
 
@@ -6,3 +7,8 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'password1', 'password2']
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'password1': forms.PasswordInput(attrs={'class': 'form-control'}),
+            'password2': forms.PasswordInput(attrs={'class': 'form-control'}),
+        }
