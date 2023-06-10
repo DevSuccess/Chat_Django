f95c60f077475596f853872cb2db2bf3dfda2f3d
ITDevSuccess <raharisontsidiany@gmail.com>
2023-06-10 21:17:15 +0300

    Ajout de quelques fonctionnalités utilitaires pour l'application de chat

--- backchat/chat/consumers.py
+++ backchat/chat/consumers.py
@@ -21,10 +21,12 @@ class ChatConsumer(AsyncWebsocketConsumer):
         await self.channel_layer.group_add(
             self.room_group_name, self.channel_name
         )
-
+        await self.add_user(self.room_name, self.user)
         await self.accept()
 
     async def disconnect(self, close_code):
+        await self.remove_user(self.room_name, self.user)
+
         await self.channel_layer.group_discard(
             self.room_group_name, self.channel_name
         )
@@ -34,6 +36,9 @@ class ChatConsumer(AsyncWebsocketConsumer):
         message = text_data_json['message']
         user = self.user
         username = user.username
+        room = self.room_name
+
+        await self.save_message(room, user, message)
         
         await self.channel_layer.group_send(
             self.room_group_name,
@@ -59,3 +64,22 @@ class ChatConsumer(AsyncWebsocketConsumer):
                 'username': username
             })
         )
+
+    @sync_to_async
+    def save_message(self, room, user, message):
+        room = Room.objects.get(slug=room)
+        Message.objects.create(room=room, user=user, message=message)
+
+    @sync_to_async
+    def add_user(self, room, user):
+        room = Room.objects.get(slug=room)
+        if user not in room.users.all():
+            room.users.add(user)
+            room.save()
+
+    @sync_to_async
+    def remove_user(self, room, user):
+        room = Room.objects.get(slug=room)
+        if user in room.users.all():
+            room.users.remove(user)
+            room.save()
