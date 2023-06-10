d22a02f1e80143e4f7857a5d792716ab92f7f9ba
ITDevSuccess <raharisontsidiany@gmail.com>
2023-06-10 19:45:23 +0300

    Ecrire des consommateurs et des routeurs pour WebSockets

---
+++ backchat/chat/consumers.py
@@ -0,0 +1,55 @@
+import json
+
+from asgiref.sync import sync_to_async
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+from chat.models import Room, Message
+
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(args, kwargs)
+        self.user = None
+        self.room_group_name = None
+        self.room_name = None
+
+    async def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_slug']
+        self.room_group_name = f"chat_{self.room_name}"
+        self.user = self.scope['user']
+
+        await self.channel_layer.group_add(
+            self.room_group_name, self.channel_name
+        )
+
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.room_group_name, self.channel_name
+        )
+
+    async def receive(self, text_data=None, byte_date=None):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+        username = self.user.username
+        
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message,
+                'username': username
+            }
+        )
+    
+    async def chat_message(self, event):
+        message = event['message']
+        username = event['username']
+        
+        await self.send(
+            text_data= json.dumps({
+                'message': message,
+                'username': username
+            })
+        )
